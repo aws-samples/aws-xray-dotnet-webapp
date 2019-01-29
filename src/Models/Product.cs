04033354386161de2707f723bb8afd0dad3a7aae
Yogiraj Awati <awati@amazon.com>
2019-01-29 12:45:49 -0800

    deleting old app

--- src/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SampleEBWebApplication.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
