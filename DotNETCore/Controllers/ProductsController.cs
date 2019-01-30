804a43301bed1864067cad5f62f998a36e9b671b
Yogiraj Awati <awati@amazon.com>
2019-01-30 11:30:56 -0800

    Adding .NET Core sample app

---
+++ DotNETCore/Controllers/ProductsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Net;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Util;
+using Amazon.XRay.Recorder.Core;
+using Amazon.XRay.Recorder.Handlers.SqlServer;
+using Amazon.XRay.Recorder.Handlers.System.Net;
+using Microsoft.AspNetCore.Mvc;
+using SampleEBASPNETCoreApplication.Models;
+
+namespace SampleEBASPNETCoreApplication.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Products")]
+    public class ProductsController : Controller
+    {
+        static ProductsController()
+        {
+           
+            LazyDdbClient = new Lazy<AmazonDynamoDBClient>(() =>
+            {
+                var client = new AmazonDynamoDBClient(EC2InstanceMetadata.Region ?? RegionEndpoint.USWest2);
+
+                return client;
+            });
+
+            LazyTable = new Lazy<Table>(() =>
+            {
+                var tableName = "SampleProduct";
+                return Table.LoadTable(LazyDdbClient.Value, tableName);
+            });
+
+        }
+        private static readonly Lazy<AmazonDynamoDBClient> LazyDdbClient;
+        private static readonly Lazy<Table> LazyTable;
+        // GET: api/Products
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            try
+            {
+                AWSXRayRecorder.Instance.AddAnnotation("Get", id);
+
+                var product = AWSXRayRecorder.Instance.TraceMethod<Product>("QueryProduct", () => QueryProduct(id));
+
+                // Trace out-going HTTP request
+                AWSXRayRecorder.Instance.TraceMethod("Outgoing Http Web Request", () => MakeHttpWebRequest(id));
+
+                CustomSubsegment(); // create custom Subsegment
+
+                // Trace SQL query
+                //  AWSXRayRecorder.Instance.TraceMethod("Query SQL", () => QuerySql(id));
+
+                return product.ToString();// Ok(product);
+            }
+            catch (ProductNotFoundException e)
+            {
+                return "Product not found !";// NotFound();
+            }
+        }
+
+        private void CustomSubsegment()
+        {
+            try
+            {
+                AWSXRayRecorder.Instance.BeginSubsegment("CustomSubsegment");
+            }
+            catch (Exception e)
+            {
+                AWSXRayRecorder.Instance.AddException(e);
+            }
+            finally
+            {
+                AWSXRayRecorder.Instance.EndSubsegment();
+            }
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public void Post(Product product)//[FromBody]string value)
+        {
+           AWSXRayRecorder.Instance.TraceMethodAsync("AddProduct", () => AddProduct<Document>(product));
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        private static void MakeHttpWebRequest(int id)
+        {
+            AWSXRayRecorder.Instance.AddAnnotation("WebRequestCall", id);
+            HttpWebRequest request = null;
+            request = (HttpWebRequest) WebRequest.Create("http://www.amazon.com");
+            request.GetResponseTraced();
+        }
+
+        private Product QueryProduct(int id)
+        {
+            var item = LazyTable.Value.GetItemAsync(id).Result;
+            if (item == null)
+            {
+                throw new ProductNotFoundException("Can't find a product with id = " + id);
+            }
+
+            return BuildProduct(item);
+        }
+
+        private async Task<Document> AddProduct<TResult>(Product product)
+        {
+            var document = new Document();
+            document["Id"] = product.Id;
+            document["Name"] = product.Name;
+            document["Price"] = product.Price;
+
+            return await LazyTable.Value.PutItemAsync(document);
+        }
+
+        private Product BuildProduct(Document document)
+        {
+            var product = new Product();
+            product.Id = document["Id"].AsInt();
+            product.Name = document["Name"].AsString();
+            product.Price = document["Price"].AsDecimal();
+            return product;
+        }
+
+        private void QuerySql(int id)
+        {
+            var connectionString = ""; // Configure Connection string -> Format : "Data Source=(RDS endpoint),(port number);User ID=(your user name);Password=(your password);"
+            using (var sqlConnection = new SqlConnection(connectionString))
+            using (var sqlCommand = new TraceableSqlCommand("SELECT " + id, sqlConnection))
+            {
+                sqlCommand.Connection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+        private class ProductNotFoundException : Exception
+        {
+            public ProductNotFoundException(string message) : base(message) { }
+        }
+    }
+}
---
+++ DotNETCore/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Reflection;
+using Amazon;
+using Amazon.XRay.Recorder.Core;
+using Amazon.XRay.Recorder.Handlers.AwsSdk;
+using log4net;
+using log4net.Config;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SAmpleEBASPNETCoreApplication
+{
+    public class Startup
+    {
+        public static ILog log;
+        public IConfiguration Configuration { get; }
+
+        static Startup() // create log4j instance
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            log = LogManager.GetLogger(typeof(Startup));
+            AWSXRayRecorder.RegisterLogger(LoggingOptions.Log4Net);
+        }
+
+
+        public Startup(IConfiguration configuration)
+        {
+            AWSXRayRecorder.InitializeInstance(configuration: Configuration); // Inititalizing Configuration object with X-Ray recorder
+            AWSSDKHandler.RegisterXRayForAllServices(); // All AWS SDK requests will be traced
+        }
+      
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseXRay("ASPNETCoreSampleApp"); // Integrate ASPNETCore app with X-Ray
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
